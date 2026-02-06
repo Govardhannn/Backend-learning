7e00774bb476dd64a0ffc2086c690850cd29885e
Govardhannn <sahanigovardhan69@gmail.com>
2026-02-06 17:01:58 +0000

    Razorpay implemented

--- razerpay-backend/src/controllers/payment.controller.js
+++ razerpay-backend/src/controllers/payment.controller.js
@@ -1,7 +1,9 @@
-require('dotenv').config();
-const Razorpay = require('razorpay');
+import productModel from "../models/product.model.js";
+import Razorpay from "Razorpay";
 
-const razorpay = new Razorpay({
+const Razorpay = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
   key_secret: process.env.RAZORPAY_KEY_SECRET,
 });
+
+
